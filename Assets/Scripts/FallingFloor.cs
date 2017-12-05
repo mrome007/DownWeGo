b318a892068054896b75bad2582c4f19f79189e4
mrome007 <mrome007@ucr.edu>
2017-12-04 21:24:36 -0800

    Initial commit

---
+++ Assets/Scripts/FallingFloor.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingFloor : MonoBehaviour
+{
+    public int TurnsToFall = 2;
+
+    private int currentTurn = 0;
+
+    private const float FallOffset = 0.01f;
+    private const float FallTime = 2f;
+    private const float GoDownDistance = 0.5f;
+    private const float FallDistance = 30f;
+
+    private void Start()
+    {
+        currentTurn = 0;
+    }
+
+    public void HandleNextTurn()
+    {
+        currentTurn++;
+
+        if(currentTurn >= TurnsToFall)
+        {
+            Fall();
+        }
+        else
+        {
+            GoDown();
+        }
+    }
+
+    private void GoDown()
+    {
+        StartCoroutine(DownMovement(GoDownDistance));
+    }
+
+    private void Fall()
+    {
+        StartCoroutine(DownMovement(FallDistance));
+    }
+
+    private IEnumerator DownMovement(float downDistance)
+    {
+        var targetYPos = transform.position.y - downDistance;
+        var speed = downDistance / FallTime;
+        while(transform.position.y >= targetYPos + FallOffset)
+        {
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = new Vector3(transform.position.x, targetYPos, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/TurnsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnsController : MonoBehaviour 
+{
+    [SerializeField]
+    private List<FallingFloor> FallingFloors;
+
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            NextTurn();
+        }
+    }
+
+    private void NextTurn()
+    {
+        for(int index = 0; index < FallingFloors.Count; index++)
+        {
+            FallingFloors[index].HandleNextTurn();
+        }
+    }
+}
